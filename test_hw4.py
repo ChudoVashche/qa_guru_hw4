2a532e6fb67b92c425153598bb1ac5816514e6a4
Anna Akatyeva <akatyeva_a@mail.ru>
2023-04-13 20:00:50 +0300

    исправила отступы, добавила вывод на печать некоторых результатов

--- test_hw4.py
+++ test_hw4.py
@@ -79,6 +79,7 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = dict(zip(first, second))
-    print(d)
+    for key, value in d.items():
+        print(value)
     assert isinstance(d, dict)
     assert len(d) == 5
